155242edff8aa45247045a6760882716f36470d5
Mauricio Lopes <mlopeesz@gmail.com>
2022-07-08 14:19:21 -0300

    feat: rotas de login

---
+++ app/backend/src/routes/loginRoutes.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+import UserController from '../controllers/UserController';
+
+const loginRoutes = Router();
+const controller = new UserController();
+
+loginRoutes.post('/', controller.login);
+
+export default loginRoutes;
