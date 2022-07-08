95aaf39def2d16e75b2989468e90d1a0da8c236d
Mauricio Lopes <mlopeesz@gmail.com>
2022-07-08 14:18:41 -0300

    feat: userService

---
+++ app/backend/src/services/userService.ts
@@ -0,0 +1,30 @@
+import CustomError from '../helpers/CustomError';
+import { ILogin } from '../interfaces';
+import Bcrypt from '../auth/bcrypt';
+import JWT from '../auth/jwt';
+import Model from '../database/models/user';
+
+export default class UserService {
+  private model = Model;
+  private jwt = new JWT();
+  private bcrypt = new Bcrypt();
+
+  async login(loginBody: ILogin) {
+    const userData = await this.model.findOne({
+      where: { email: loginBody.email },
+    });
+    if (!userData) {
+      throw new CustomError(401, 'Incorrect email or password');
+    }
+
+    const { username, email, password, role } = userData;
+    const validatePassword = await this.bcrypt.comparePassword(
+      loginBody.password,
+      password,
+    );
+    if (!validatePassword) {
+      throw new CustomError(401, 'Incorrect email or password');
+    }
+    return this.jwt.generateToken({ role, username, email });
+  }
+}
