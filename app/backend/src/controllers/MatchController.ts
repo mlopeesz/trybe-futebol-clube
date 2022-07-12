9326edf366a9b8fae60151e3d767c13a51951aab
Mauricio Lopes <mlopeesz@gmail.com>
2022-07-12 17:43:47 -0300

    feat: implementa funções create e finish / Matches

--- app/backend/src/controllers/MatchController.ts
+++ app/backend/src/controllers/MatchController.ts
@@ -6,12 +6,32 @@ export default class MatchController {
   constructor(service: MatchService) {
     this.service = service;
     this.getAll = this.getAll.bind(this);
+    this.create = this.create.bind(this);
+    this.finishMatch = this.finishMatch.bind(this);
   }
 
   async getAll(_req: Request, res: Response, next: NextFunction) {
     try {
       const matches = await this.service.getAll();
       return res.status(200).json(matches);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async create(req: Request, res: Response, next: NextFunction) {
+    try {
+      const createdMatch = await this.service.create(req.body);
+      return res.status(201).json(createdMatch);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async finishMatch(req: Request, res: Response, next: NextFunction) {
+    try {
+      await this.service.finishMatch(req.params.id);
+      return res.status(200).json({ message: 'Finished' });
     } catch (error) {
       next(error);
     }
--- app/backend/src/services/MatchService.ts
+++ app/backend/src/services/MatchService.ts
@@ -1,5 +1,8 @@
+import { Identifier } from 'sequelize/types';
 import team from '../database/models/team';
 import Model from '../database/models/match';
+import { IMatch } from '../interfaces';
+import CustomError from '../helpers/CustomError';
 
 export default class MatchService {
   private model = Model;
@@ -12,5 +15,29 @@ export default class MatchService {
       ],
     });
     return matches;
+  }
+
+  async create(matchData: IMatch) {
+    const { homeTeam, awayTeam } = matchData;
+
+    if (homeTeam === awayTeam) {
+      throw new CustomError(401, 'It is not possible to create a match with two equal teams');
+    }
+
+    const homeTeamFind = await this.model.findOne({ where: { id: homeTeam } });
+    const awayTeamFind = await this.model.findOne({ where: { id: awayTeam } });
+
+    if (!homeTeamFind || !awayTeamFind) {
+      throw new CustomError(404, 'There is no team with such id!');
+    }
+
+    const createdMatch = await this.model.create({ ...matchData });
+
+    return createdMatch;
+  }
+
+  async finishMatch(id: Identifier | undefined) {
+    const finishUpdate = this.model.update({ inProgress: false }, { where: { id } });
+    return finishUpdate;
   }
 }
