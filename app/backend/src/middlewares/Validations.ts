d4c76e82f9ec7c6bdfd81a23eae7cac0a06b8daf
Mauricio Lopes <mlopeesz@gmail.com>
2022-07-13 10:11:13 -0300

    feat: implementa validação de token antes de criar uma partida

--- app/backend/src/middlewares/Validations.ts
+++ app/backend/src/middlewares/Validations.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
+import JWT from '../auth/jwt';
 import loginSchema from '../schemas/loginSchema';
 
 export default class Validations {
@@ -6,6 +7,14 @@ export default class Validations {
     const { error } = loginSchema.validate(req.body);
     if (error) {
       return res.status(400).json({ message: 'All fields must be filled' });
+    }
+    next();
+  };
+
+  public validateToken = async (req: Request, res: Response, next: NextFunction) => {
+    const jwt = new JWT();
+    if (!jwt.validateToken(req.headers.authorization as string)) {
+      return next({ status: 401, message: 'Token must be a valid token' });
     }
     next();
   };
--- app/backend/src/routes/matchRoutes.ts
+++ app/backend/src/routes/matchRoutes.ts
@@ -1,13 +1,15 @@
 import { Router } from 'express';
 import MatchService from '../services/MatchService';
 import MatchController from '../controllers/MatchController';
+import Validations from '../middlewares/Validations';
 
 const route = Router();
 const service = new MatchService();
 const controller = new MatchController(service);
+const validation = new Validations();
 
 route.get('/', controller.getAll);
-route.post('/', controller.create);
+route.post('/', validation.validateToken, controller.create);
 route.patch('/:id/finish', controller.finishMatch);
 
 export default route;
